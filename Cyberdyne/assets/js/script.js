782174502feb0da83e0cc937201daa239918f63e
João Uninter <contato.jsbarbosa@gmail.com>
2022-12-23 19:02:58 -0300

    Adicionado JavaScrip de alguns banners

--- Cyberdyne/assets/js/script.js
+++ Cyberdyne/assets/js/script.js
@@ -8,3 +8,59 @@ window.onload = function () {
   });
 };
 
+
+/* banners*/
+var indexb = 0;
+banners();
+function banners(){
+  var i;
+  var x = document.getElementsByClassName('slide-banner');
+  for (i =0; i< x.length; i++){
+    x[i].style.display = "none";
+
+  }
+  indexb++;
+  if(indexb > x.length){
+    indexb =1
+  }
+  x[indexb-1].style.display = "block";  
+  setTimeout(banners, 2200);
+}
+
+/* avaliação*/
+
+var indexr = 0;
+reviews();
+function reviews(){
+  var i;
+  var x = document.getElementsByClassName('slide-reviews');
+  for (i =0; i< x.length; i++){
+    x[i].style.display = "none";
+
+  }
+  indexr++;
+  if(indexr > x.length){
+    indexr =1
+  }
+  x[indexr-1].style.display = "block";  
+  setTimeout(reviews, 2200);
+}
+
+/* tec*/
+
+var indext = 0;
+tech();
+function tech(){
+  var i;
+  var x = document.getElementsByClassName('slide-tech');
+  for (i =0; i< x.length; i++){
+    x[i].style.display = "none";
+
+  }
+  indext++;
+  if(indext > x.length){
+    indext =1
+  }
+  x[indext-1].style.display = "block";  
+  setTimeout(tech, 1200);
+}
